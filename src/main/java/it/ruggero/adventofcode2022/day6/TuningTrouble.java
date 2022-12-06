9359a20811df54a02296902c0d280e0f495fb76f
ruggeromontesi <ruggero.montesi1@gmail.com>
2022-12-06 11:13:54 +0200

    Day 6,  refactored

--- src/main/java/it/ruggero/adventofcode2022/day6/TuningTrouble.java
+++ src/main/java/it/ruggero/adventofcode2022/day6/TuningTrouble.java
@@ -1,48 +1,14 @@
 package it.ruggero.adventofcode2022.day6;
 
-import lombok.Getter;
-
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class TuningTrouble implements BiFunction<String,Integer,Integer> {
-
-
-
-    @Getter
-    public static Function<String, Integer> findStartOfPacketMarker = s -> {
-        for (int k = 3; k + 3 < s.length(); k++) {
-            String movingWindow = s.substring(k - 3, k + 1);
-            var result = IntStream.range(0, movingWindow.length())
-                    .mapToObj(movingWindow::charAt).collect(Collectors.toSet());
-            if (result.size() == 4) {
-                return k + 1;
-            }
-        }
-
-        return -1;
-    };
-
-
-    @Getter
-    public static Function<String, Integer> findStartOfPacketMarkerPartTwo = s -> {
-        for (int k = 13; k + 13 < s.length(); k++) {
-            String movingWindow = s.substring(k - 13, k + 1);
-            var result = IntStream.range(0, movingWindow.length())
-                    .mapToObj(movingWindow::charAt).collect(Collectors.toSet());
-            if (result.size() == 14) {
-                return k + 1;
-            }
-        }
-
-        return -1;
-    };
+public class TuningTrouble implements BiFunction<String, Integer, Integer> {
 
     @Override
     public Integer apply(String s, Integer numberOfIdenticalCharacters) {
-        for (int k = numberOfIdenticalCharacters - 1 ; k + numberOfIdenticalCharacters - 1 < s.length(); k++) {
+        for (int k = numberOfIdenticalCharacters - 1; k + numberOfIdenticalCharacters - 1 < s.length(); k++) {
             String movingWindow = s.substring(k - numberOfIdenticalCharacters + 1, k + 1);
             var result = IntStream.range(0, movingWindow.length())
                     .mapToObj(movingWindow::charAt).collect(Collectors.toSet());
@@ -50,7 +16,6 @@ public class TuningTrouble implements BiFunction<String,Integer,Integer> {
                 return k + 1;
             }
         }
-
         return -1;
     }
 }
--- src/test/java/it/ruggero/adventofcode2022/day6/TuningTroubleTest.java
+++ src/test/java/it/ruggero/adventofcode2022/day6/TuningTroubleTest.java
@@ -13,14 +13,13 @@ public class TuningTroubleTest {
     private static final String EXAMPLE_3 = "nppdvjthqldpwncqszvftbrmjlhg";
     private static final String EXAMPLE_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
     private static final String EXAMPLE_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
-
     private static final String FILE_PATH = ".\\src\\main\\resources\\adventofcode2022\\day6\\Day6.txt";
-
+    private static final int NUMBER_OF_CHARS_PART_1 = 4;
+    private static final int NUMBER_OF_CHARS_PART_2 = 14;
     private static final int RESULT_PART_1 = 1896;
     private static final int RESULT_PART_2 = 3452;
 
     private TuningTrouble tuningTrouble;
-
 
     @BeforeEach
     void prepare() {
@@ -29,42 +28,28 @@ public class TuningTroubleTest {
 
     @Test
     void shouldFindStartOfPacket() {
-        int startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(EXAMPLE_1);
+        int startingIndex = tuningTrouble.apply(EXAMPLE_1, NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(7);
-        startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(EXAMPLE_2);
+        startingIndex = tuningTrouble.apply(EXAMPLE_2, NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(5);
-        startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(EXAMPLE_3);
+        startingIndex = tuningTrouble.apply(EXAMPLE_3, NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(6);
-        startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(EXAMPLE_4);
+        startingIndex = tuningTrouble.apply(EXAMPLE_4, NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(10);
-        startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(EXAMPLE_5);
+        startingIndex = tuningTrouble.apply(EXAMPLE_5, NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(11);
-
-        startingIndex = tuningTrouble.apply(EXAMPLE_1,4);
-        assertThat(startingIndex).isEqualTo(7);
     }
-
 
     @Test
     void shouldFindStartOfPacketActualFile() {
-        var line = getLinesFromFile(FILE_PATH).get(0);
-        int startingIndex = TuningTrouble.getFindStartOfPacketMarker().apply(line);
+        int startingIndex = tuningTrouble.apply(getLinesFromFile(FILE_PATH).get(0), NUMBER_OF_CHARS_PART_1);
         assertThat(startingIndex).isEqualTo(RESULT_PART_1);
     }
-
-    @Test
-    void shouldFindStartOfPacketActualFileImproved() {
-        var line = getLinesFromFile(FILE_PATH).get(0);
-        int startingIndex = (new TuningTrouble()).apply(line, 4);
-        assertThat(startingIndex).isEqualTo(RESULT_PART_1);
-    }
-
-
 
     @Test
     void shouldFindStartOfPacketActualFilePartTwo() {
         var line = getLinesFromFile(FILE_PATH).get(0);
-        int startingIndex = TuningTrouble.getFindStartOfPacketMarkerPartTwo().apply(line);
+        int startingIndex = tuningTrouble.apply(line, NUMBER_OF_CHARS_PART_2);
         assertThat(startingIndex).isEqualTo(RESULT_PART_2);
     }
 }
